0fd5d84ec548fd28464d73598e191a14c7c467a0
Akshata72 <sableakshata880@gmail.com>
2022-02-24 22:16:06 +0530

    Added:UC6 logic

--- arithmetic.sh
+++ arithmetic.sh
@@ -10,3 +10,11 @@ echo $a"*"$b"+"$c"="$(($a*$b+$c))
 echo $c"+"$a"/"$b"="$((($c+$a)/$b))
 
 echo $a"%"$b"+"$c"="$((($a%$b)+$c))
+
+
+arithmetic[am]="((($a+$b)*$c))"
+arithmetic[ma]="(($a*$b+$c))"
+arithmetic[ad]="((($c+$a)/$b))"
+arithmetic[pa]="((($a%$b)+$c))"
+
+echo ${arithmetic[@]}
