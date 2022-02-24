71253d59d0a9c9fb22e996d175463f170fb58aa7
Akshata72 <sableakshata880@gmail.com>
2022-02-24 22:23:27 +0530

    Added:UC7 logic

--- arithmetic.sh
+++ arithmetic.sh
@@ -11,10 +11,18 @@ echo $c"+"$a"/"$b"="$((($c+$a)/$b))
 
 echo $a"%"$b"+"$c"="$((($a%$b)+$c))
 
-
+#store dictionary 
 arithmetic[am]="((($a+$b)*$c))"
 arithmetic[ma]="(($a*$b+$c))"
 arithmetic[ad]="((($c+$a)/$b))"
 arithmetic[pa]="((($a%$b)+$c))"
 
+echo ${arithmetic[@]}
+
+
+#read from dictionary to array
+arithmetic[0]=${arithmetic[am]}
+arithmetic[1]=${arithmetic[ma]}
+arithmetic[2]=${arithmetic[ad]}
+arithmetic[3]=${arithmetic[pa]}
 echo ${arithmetic[@]}
